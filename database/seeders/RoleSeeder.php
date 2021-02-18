070af3c72f61b4f97c021948aca339deefc788f1
ismatazmy@live.com <I5mat@zmy>
2021-02-18 16:14:04 +0800

    2nd commit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Role::factory()->times(10)->create();
+
+        DB::table('roles')->insert([
+            'name' => 'Reseller'
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'Distributor'
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'User'
+        ]);
     }
 }
