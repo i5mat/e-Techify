4d6c0e586e4ed3c4cda945704364ed52288e1c01
Wan Ismat <ismatazmy@live.com>
2021-07-17 18:36:14 +0800

    153th push, 17-7-2021.
    Shop pagination testing.
    
    On-Plan
    1. Serial Number alter/update.
    
    System Progress => 98.5%

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
 
         if($this->app->environment('production')) {
             \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
         }
     }
 }
