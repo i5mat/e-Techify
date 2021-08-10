528c9a8a39db10b51e182ff6b86e80aaf1760c81
Wan Ismat <ismatazmy@live.com>
2021-08-11 02:17:17 +0800

    195th push, 11-8-2021.
    E-mail header URL changed.
    
    System Progress => 99.5%

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'e-Techify')
-<img src="http://127.0.0.1:8000/image/XT-logo-email.png" class="logo" alt="Laravel Logo">
+<img src="image/XT-logo-email.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
