dba3f173c19bb7ff84cb07725835134ce430275e
Wan Ismat <ismatazmy@live.com>
2021-08-30 20:54:30 +0800

    200th push, 14-8-2021.
    MINOR FIX
    
    System Progress => 99.5%

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -253,21 +253,7 @@
         </div>
 
         <script>
-            @can('is-user')
-                //Start of Tawk.to Script
-
-                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                (function(){
-                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                    s1.async=true;
-                    s1.src='https://embed.tawk.to/60befaa64ae6dd0abe7d09f4/1f7l0673b';
-                    s1.charset='UTF-8';
-                    s1.setAttribute('crossorigin','*');
-                    s0.parentNode.insertBefore(s1,s0);
-                })();
-
-                //End of Tawk.to Script
-            @endcan
+            console.log('%cStop!', 'color: red; font-size: 30px; font-weight: bold;');
 
             $('#exampleModal').on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget) // Button that triggered the modal
